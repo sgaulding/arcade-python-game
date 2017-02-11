e11428f399b29fea721ee5ba58734b037ae692d0
Sam Gaulding <sam.gaulding@gmail.com>
2017-02-11 15:41:34 -0600

    Add Background

--- game.py
+++ game.py
@@ -2,6 +2,11 @@ import arcade as arcade
 
 
 class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.AMAZON)
+
     def on_draw(self):
         arcade.start_render()
         arcade.draw_text('Hello', 100, 100, arcade.color.WHITE)
