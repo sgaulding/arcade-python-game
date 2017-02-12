a03b4704e183f3890368e6ba725042427e5ad0c9
Sam Gaulding <sam.gaulding@gmail.com>
2017-02-11 19:22:28 -0600

    Add Player

--- game.py
+++ game.py
@@ -2,19 +2,30 @@ import arcade as arcade
 
 
 class MyGame(arcade.Window):
-
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.AMAZON)
         self.score = 0
 
+        self.all_sprites_list = arcade.SpriteList()
+        self.player_sprite = arcade.Sprite('images/zombie_stand.png', 0.6)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.all_sprites_list.append(self.player_sprite)
+
     def on_draw(self):
         arcade.start_render()
         output = f'Score: {self.score:02d}'
         arcade.draw_text(output, 100, 100, arcade.color.WHITE)
+        self.all_sprites_list.draw()
 
     def animate(self, delta_time):
         self.score += 1
+        self.all_sprites_list.update()
+
+    def on_mouse_motion(self, x, y, deltaX, deltaY):
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
 
 
 def main():
